3f79069d84f00f33b0d6492afd99d3f7f43a8cd2
al <alvminvm@qq.com>
2025-08-22 10:12:51 +0800

    mod: Result 增加预定义

--- src/main/kotlin/com/krzhi/utils/Result.kt
+++ src/main/kotlin/com/krzhi/utils/Result.kt
@@ -5,6 +5,10 @@ data class Result(
     var msg: String,
     var data: Any? = null
 ) {
+    init {
+        // todo: 加密返回
+    }
+
     companion object {
         fun success(data: Any? = null): Result {
             return Result(0, "success", data)
@@ -13,5 +17,9 @@ data class Result(
         fun fail(msg: String): Result {
             return Result(-1, msg)
         }
+
+        fun illegalArgs() = Result(-2, "参数有误")
+
+        fun systemError() = Result(-3, "系统错误，请稍候重试")
     }
 }
