c0aafadc2e5bb48ccb9900bc632c75682f159266
al <alvminvm@qq.com>
2025-08-27 17:33:24 +0800

    improve: 完善 jwt

--- src/main/kotlin/com/krzhi/utils/auth/Jwt.kt
+++ src/main/kotlin/com/krzhi/utils/auth/Jwt.kt
@@ -1,47 +1,58 @@
 package com.krzhi.utils.auth
 
-import com.google.gson.Gson
-import com.krzhi.utils.crypt.AesUtils
-import com.krzhi.utils.crypt.RsaUtils
-import org.jose4j.jwk.RsaJsonWebKey
-import org.jose4j.jws.AlgorithmIdentifiers
-import org.jose4j.jws.JsonWebSignature
-import org.jose4j.jwt.JwtClaims
+import com.krzhi.utils.annotation.Slf4j
+import com.krzhi.utils.annotation.Slf4j.Companion.log
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.util.*
+import kotlin.time.Duration.Companion.days
 
+@Slf4j
 @Component
 class Jwt {
     companion object {
-        private val gson = Gson()
+        private const val CLAIM_PRO_EXPIRE_AT = "pexpat"
     }
 
     @Value("\${jwt.aes-key}")
     private lateinit var aesKey: String
-    @Value("\${jwt.key-id}")
-    private lateinit var keyId: String
-    @Value("\${jwt.private-key}")
-    private lateinit var privateKey: String
+    @Value("\${jwt.secret-key}")
+    private lateinit var secretKey: String
 
     fun createAuthToken(info: AuthInfo): String {
-        val claims = JwtClaims().apply {
-            setGeneratedJwtId()
-            setIssuedAtToNow()
-            setExpirationTimeMinutesInTheFuture(30 * 24 * 60f)
-            setNotBeforeMinutesInThePast(1f)
-            subject = "u:${info.userId}"
-            audience = listOf("auth")
+        return Jwts.builder()
+            .setIssuedAt(Date())
+            .setSubject("${info.userId}")
+            .setAudience("user")
+            .setExpiration(Date(System.currentTimeMillis() + 30.days.inWholeMilliseconds))
+            .claim(CLAIM_PRO_EXPIRE_AT, info.proExpireAt)
+            .signWith(SignatureAlgorithm.HS512, secretKey)
+            .compact()
+    }
 
-            setClaim("auth", AesUtils.CBC.encrypt(gson.toJson(info), aesKey))
+    fun parseAuthToken(token: String): AuthInfo? {
+        return try {
+            val claims = parseJwt(token) ?: return null
+            if (claims.audience != "user") return null
+            AuthInfo(
+                userId = claims.subject.toLong(),
+                proExpireAt = claims.get(CLAIM_PRO_EXPIRE_AT, Long::class.java),
+            )
+        } catch (t: Throwable) {
+            log.error("parse auth token exception", t)
+            null
         }
+    }
 
-        val signature = JsonWebSignature().apply {
-            algorithmHeaderValue = AlgorithmIdentifiers.RSA_USING_SHA256
-            keyIdHeaderValue = keyId
-            payload = claims.toJson()
-            key = RsaUtils.getPrivateKey(privateKey)
+    private fun parseJwt(token: String): Claims? {
+        return try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)?.body
+        } catch (t: Throwable) {
+            log.warn("JWT token verification failed", t)
+            null
         }
-
-        return signature.compactSerialization
     }
 }
