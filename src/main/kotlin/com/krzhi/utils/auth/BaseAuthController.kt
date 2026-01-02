fb83528c962f128768e22a8c52ce7f6366ce2a79
al <alvminvm@qq.com>
2026-01-02 15:04:49 +0800

    userAuthInfoOrNull

--- src/main/kotlin/com/krzhi/utils/auth/BaseAuthController.kt
+++ src/main/kotlin/com/krzhi/utils/auth/BaseAuthController.kt
@@ -3,6 +3,8 @@ package com.krzhi.utils.auth
 import org.springframework.security.core.context.SecurityContextHolder
 
 abstract class BaseAuthController {
-    val userAuthInfo: UserAuthInfo
-        get() = SecurityContextHolder.getContext().authentication.principal as UserAuthInfo
+    val userAuthInfo: UserAuthInfo get() = userAuthInfoOrNull!!
+
+    val userAuthInfoOrNull: UserAuthInfo?
+        get() = SecurityContextHolder.getContext().authentication.principal as? UserAuthInfo
 }
