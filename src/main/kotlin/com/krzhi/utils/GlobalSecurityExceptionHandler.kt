3980c255c9cb5d8d5bf7a3e882987891069c4347
al <alvminvm@qq.com>
2026-01-03 15:56:05 +0800

    全局异常处理

---
+++ src/main/kotlin/com/krzhi/utils/GlobalSecurityExceptionHandler.kt
@@ -0,0 +1,23 @@
+package com.krzhi.utils
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authorization.AuthorizationDeniedException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalSecurityExceptionHandler {
+    // 捕获权限不足异常
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDenied(e: AccessDeniedException, response: HttpServletResponse): Result<*> {
+        response.status = HttpServletResponse.SC_FORBIDDEN
+        return Result.denied<Unit>()
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException::class)
+    fun handleAccessDenied(e: AuthorizationDeniedException, response: HttpServletResponse): Result<*> {
+        response.status = HttpServletResponse.SC_FORBIDDEN
+        return Result.denied<Unit>()
+    }
+
+}
